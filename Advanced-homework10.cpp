a37b83598d80375fbd283a2cd994352a213727bd
XiaoHsiang <48340410+XiaoHsiang@users.noreply.github.com>
2019-04-26 05:36:06 +0800

    Add files via upload

---
+++ Advanced-homework10.cpp
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+void Factorization(int);
+int main(void){
+	int input_num;
+	int divisor;
+	printf("½Ð¿é¤J¼Æ¦r:");
+	scanf(" %d",&input_num);
+	
+	Factorization(input_num);
+	
+	return 0;
+	system("pause");
+}
+void Factorization(int input_num){
+	int divisor=2;
+	while(input_num/divisor>=1){
+	 
+		if(input_num%divisor==0){
+			
+			if(input_num/divisor==1){
+				printf("%d",divisor);
+				break;
+			}
+			
+			input_num/=divisor;
+			printf("%d*",divisor);
+			
+		}
+		
+		else{
+			divisor+=1;
+		}
+		
+	}
+}
